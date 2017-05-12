81a24b733b118653a90740a47fa41298ec8a2248
roto3 <cludefanslynxoficial@gmail.com>
2017-05-12 23:50:01 +0200

    1

---
+++ CalculadoraLetraDNI/VentanaLetraDNI.java
@@ -0,0 +1,66 @@
+package Ventana;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import java.awt.Component;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+
+/*
+ * By Paco Gomez
+ * Esta ventana tendrá dos JTextFields
+ * El primero recojerá el DNI
+ * El segundo calculará la letra al apretar el botón
+ * 
+ * */
+public class VentanaLetraDNI extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+
+
+
+	/**
+	 * Create the frame.
+	 */
+	public VentanaLetraDNI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
+		
+		JLabel lblNewLabel = new JLabel("Introduce DNI");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		lblNewLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+		contentPane.add(lblNewLabel);
+		
+		textField = new JTextField();
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Letra DNI calculada:");
+		lblNewLabel_1.setAlignmentX(Component.CENTER_ALIGNMENT);
+		contentPane.add(lblNewLabel_1);
+		
+		textField_1 = new JTextField();
+		textField_1.setEditable(false);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Calcular");
+		btnNewButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+		contentPane.add(btnNewButton);
+	}
+
+}
